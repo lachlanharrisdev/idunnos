1f86a1ee3416c1160bec1c0128de36d0e1442dd6
Lachlan Harris <121010139+lachlanharrisdev@users.noreply.github.com>
2026-02-02 15:38:01 +1100

    feat: refactor for separated bootloader and kernel

---
+++ src/bootloader/boot.asm
@@ -0,0 +1,90 @@
+; ./main.asm
+
+; Copyright © Lachlan Harris 2025.
+; SPDX-License-Identifier: MIT
+
+%define ENDL 0x0D, 0x0A
+
+; FAT12 Header
+jmp short start
+nop
+
+bdb_oem:                    db 'MSWIN4.1'           ; 8 bytes
+bdb_bytes_per_sector:       dw 512
+bdb_sectors_per_cluster:    db 1
+bdb_reserved_sectors:       dw 1
+bdb_fat_count:              db 2
+bdb_dir_entries_count:      dw 0E0h
+bdb_total_sectors:          dw 2880                 ; 2880 * 512 = 1.44mb
+bdb_media_descriptor_type:  db 0F0h                 ; F0 = 3.5" floppy
+bdb_sectors_per_fat:        dw 9
+bdb_sectors_per_track:      dw 18
+bdb_heads:                  dw 2
+bdb_hidden_sectors:         dd 0
+bdb_large_sector_count:     dd 0
+
+; extended boot record
+ebr_drive_number:           db 0                    ; 0x00 floppy, 0x80 hdd
+                            db 0                    ; reserved
+ebr_signature:              db 29h
+ebr_volume_id:              db 12h, 34h, 56h, 78h   ; serial number, value doesn't matter
+ebr_volume_label:           db 'IDUNNOS    '        ; 11 bytes, padded with spaces
+ebr_system_id:              DB 'FAT12   '           ; 8 bytes, ''
+
+
+org  0x7c00
+bits 16
+
+; jump to entrypoint
+start:
+    jmp main
+
+; puts prints a string to stdout
+;   * ds:si points to string
+puts:
+    ; "save" modified regs
+    push si
+    push ax
+.loop:
+    ; `lodsb` loads next character into al
+    lodsb
+    ; if char is null, finish
+    or al, al
+    jz .done
+    ; print char in al
+    mov ah, 0x0e
+    mov bh, 0
+    int 0x10
+    ; loop
+    jmp .loop
+.done:
+    ; restore modified regs
+    pop ax
+    pop si
+    ret
+
+main:
+    ; set data segments to 0
+    mov ax, 0
+    mov ds, ax
+    mov es, ax
+
+    ; stack grows downwards; point behind OS
+    mov ss, ax
+    mov sp, 0x7C00
+
+    ; print boot msg
+    mov si, szBootMsg
+    call puts
+
+    hlt
+
+.halt:
+    jmp .halt
+
+; constants
+szBootMsg: db "Hello, gang!", ENDL, 0
+
+; ---
+times 510-($-$$) db 0
+dw 0AA55h
